824d09bc37e5d94b524b23b093aac518dc0d5267
Allan Wang <wang.allan@outlook.com>
2019-03-08 21:59:33 +1100

    add side drawer, navigation bar, logo and toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const Layout = props => {
   return (
     // Toolbar, SideDrawer, Backdrop
     <React.Fragment>
       <Toolbar />
+      <SideDrawer />
+
       <main className={classes.Content}>{props.children}</main>
     </React.Fragment>
   );

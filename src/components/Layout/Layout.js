d006d6041f8816cff9b0048700d84a7391cb166a
Allan Wang <wang.allan@outlook.com>
2019-03-08 23:09:55 +1100

    side drawer ok

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = props => {
-  return (
-    // Toolbar, SideDrawer, Backdrop
-    <React.Fragment>
-      <Toolbar />
-      <SideDrawer />
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
 
-      <main className={classes.Content}>{props.children}</main>
-    </React.Fragment>
-  );
-};
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+
+  render() {
+    return (
+      // Toolbar, SideDrawer, Backdrop
+      <React.Fragment>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+
+        <main className={classes.Content}>{this.props.children}</main>
+      </React.Fragment>
+    );
+  }
+}
+
 export default Layout;
